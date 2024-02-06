55a9793a3728b44a4e220bd93a165e7f20924034
johnmichaelpplale <johnmichaelpplale@gmail.com>
2024-02-06 21:28:43 +0800

    putting variables for the alarm time

--- alarm_clock.py
+++ alarm_clock.py
@@ -4,3 +4,12 @@ import pygame # to execute our ringtone
 
 # let's initialize pygame mixer
 pygame.mixer.init()
+
+# setting variables for alarm time
+
+time = input("Enter the time you want to ring the alarm(HH:MM:SS): ")
+hour = time[0:2]
+minute = time[3:5]
+seconds = time[6:8]
+period_alarm = time[9:11].upper()
+

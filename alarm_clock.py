f5577d1620104c8d6f6da1af3d4528ac85b50749
johnmichaelpplale <johnmichaelpplale@gmail.com>
2024-02-06 20:36:54 +0800

    initializing pygame.mixir.init()

--- alarm_clock.py
+++ alarm_clock.py
@@ -1,3 +1,6 @@
 # let's import some modules that will help us to run the code
 from datetime import datetime # for the time
 import pygame # to execute our ringtone
+
+# let's initialize pygame mixer
+pygame.mixer.init()
